72c8b123fbac447de4124c6815f13e29cf9d6656
youngee2 <jr3537@naver.com>
2025-06-09 17:32:27 +0900

    레벨 2: n+1문제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +11,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    /* Lv2. n+1문제
+                1) JPQL fetch join을 사용하여 n+1문제를 해결하고 있는 TodoRepository
+                2) fetch join 대신 @EntityGraph기반의 구현으로 수정해주세요.
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+                @EntityGraph는 JPA에서 fetch join을 어노테이션으로 사용할 수 있도록 지원하는 기능.
+             */
+
+    //메서드 네이밍만으로 자동으로 쿼리가 작성.
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
