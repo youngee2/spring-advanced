07819889dd8de411204cc7a4db1a4db7abfdb9a9
youngee2 <jr3537@naver.com>
2025-06-09 17:48:48 +0900

    레벨 3-1: 테스트 코드 연습

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+/* Lv3-1. 테스트코드 연습 -1
+            1) 예상대로 성공하는지에 대한 케이스
+            2) matches_메서드가_정상적으로_동작한다() 메서드가 의도대로 성공할 수 있게 수정해주세요.
+         */
 @ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
 
@@ -20,7 +24,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
